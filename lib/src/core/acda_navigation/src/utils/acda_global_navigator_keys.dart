e63e6e2528b53f799cfa99923f5ef6fdf1592eab
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-07-02 16:10:33 +0000

    feat: feature/ACDA-17 initial dashboard page

---
+++ lib/src/core/acda_navigation/src/utils/acda_global_navigator_keys.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ACDAGlobalNavigatorKeys {
+  const ACDAGlobalNavigatorKeys._();
+
+  static final GlobalKey<NavigatorState> rootNavigator = GlobalKey(debugLabel: 'root');
+}
---
+++ lib/src/features/dashboard/utils/constants/dashboard_assets.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DashboardAssets {
+  const DashboardAssets._();
+
+  static Widget gettingStartedImageAsset = Image.asset(
+    'assets/imgs/greeting_img.png',
+    width: 224,
+    height: 231,
+  );
+}
