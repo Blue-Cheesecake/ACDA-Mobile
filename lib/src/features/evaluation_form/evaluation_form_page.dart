bd39c39dbf659dd85f8dd1d283c73bd598a1bf6c
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-08-17 05:36:31 +0000

    Resolve ACDA-265 "Defect/ not allowed access"

---
+++ lib/src/features/evaluation_form/evaluation_form_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EvaluationFormPage extends StatelessWidget {
+  const EvaluationFormPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form'),
+      ),
+      body: const Center(
+        child: Text('Form'),
+      ),
+    );
+  }
+}
--- lib/src/features/register/registration/widgets/face_image_form/widgets/ungranted_access_popup_wd.dart
+++ lib/src/features/register/registration/widgets/face_image_form/widgets/ungranted_access_popup_wd.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../../../../../utils/utils.dart';
 
 class UngrantedAccessPopupWD extends StatelessWidget {
-  const UngrantedAccessPopupWD({required this.content, Key? key}) : super(key: key);
+  const UngrantedAccessPopupWD(
+      {required this.content, required this.requestCallbackfn, required this.updateImageCallbackfn, Key? key})
+      : super(key: key);
 
   final String content;
+  final Future<PermissionStatus> Function() requestCallbackfn;
+  final void Function() updateImageCallbackfn;
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +20,23 @@ class UngrantedAccessPopupWD extends StatelessWidget {
       actions: [
         CupertinoDialogAction(
           isDefaultAction: true,
+          onPressed: () async {
+            Navigator.of(context).pop();
+            final PermissionStatus status = await requestCallbackfn();
+            if (status == PermissionStatus.permanentlyDenied) {
+              openAppSettings();
+            }
+            if (status == PermissionStatus.granted) {
+              updateImageCallbackfn();
+            }
+          },
+          child: const Text(ACDACommonMessages.request),
+        ),
+        CupertinoDialogAction(
+          isDefaultAction: false,
+          isDestructiveAction: true,
           onPressed: () => Navigator.of(context).pop(),
-          child: const Text(ACDACommonMessages.confirm),
+          child: const Text(ACDACommonMessages.close),
         ),
       ],
     );
