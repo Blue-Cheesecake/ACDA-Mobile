f50826a80d926bf3773eef1a3c9e4e74e5a9b3f5
Sinut Wattanarporn <s_w.piepie@outlook.com.au>
2023-08-19 18:59:01 +0700

    feat: ACDA-256 update input state

--- lib/src/features/evaluation/logic/input/evaluation_form_input_state_notifier.dart
+++ lib/src/features/evaluation/logic/input/evaluation_form_input_state_notifier.dart
@@ -1,7 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'input.dart';
 
 class EvaluationFormInputStateNotifier extends StateNotifier<EvaluationFormInputState> {
   EvaluationFormInputStateNotifier() : super(EvaluationFormInputState());
+
+  void updateFullBodyImageFile(XFile? value) {
+    state = state.copyWith(fullBodyImageFile: value);
+  }
+
+  void updateUpperBodyImageFile(XFile? value) {
+    state = state.copyWith(upperBodyImageFile: value);
+  }
+
+  void updateStudentIdCardImageFile(XFile? value) {
+    state = state.copyWith(studentIdCardImageFile: value);
+  }
 }
