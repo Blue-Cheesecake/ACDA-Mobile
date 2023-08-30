0fccbef5cc80e721d971cdacd236dd5627048b33
Sinut Wattanarporn <s_w.piepie@outlook.com.au>
2023-08-30 23:51:42 +0700

    chore: ACDA-0 rm unused fn

--- lib/src/utils/common_widgets/src/acda_timer_camera/utils/functions/countdown_fn.dart
+++
@@ -1,8 +0,0 @@
-Future<void> countdownFN({required int initialValue, void Function(int currentCounter)? onCounting}) async {
-  int c = initialValue;
-  while (c >= 0) {
-    await Future.delayed(const Duration(seconds: 1));
-    c--;
-    if (onCounting != null) onCounting(c);
-  }
-}
