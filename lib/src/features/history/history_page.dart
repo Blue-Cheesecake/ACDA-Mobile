31dd519d57ee430a5b930f641a2e3b17c06bb406
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-09-11 13:51:19 +0000

    Resolve ACDA-0 "Hotfix/ flex error widget"

--- lib/src/features/history/history_page.dart
+++ lib/src/features/history/history_page.dart
@@ -13,57 +13,55 @@ class HistoryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 13),
-      child: Expanded(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                ACDAAssets.appIconPure(size: const Size(58, 58)),
-                const SizedBox(width: 10),
-                ACDAAssets.appTitle(size: const Size(90, 44)),
-              ],
-            ),
-            const SizedBox(height: 5),
-            const Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                AllFilterBoxWD(),
-                SizedBox(width: 6),
-                PassedFilterBoxWD(),
-                SizedBox(width: 6),
-                FailedFilterBoxWD(),
-              ],
-            ),
-            Consumer(
-              builder: (context, ref, child) {
-                late final BorderRadiusGeometry borderRadius;
-                const radius = Radius.circular(6);
+      child: Column(
+        children: [
+          Row(
+            children: [
+              ACDAAssets.appIconPure(size: const Size(58, 58)),
+              const SizedBox(width: 10),
+              ACDAAssets.appTitle(size: const Size(90, 44)),
+            ],
+          ),
+          const SizedBox(height: 5),
+          const Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              AllFilterBoxWD(),
+              SizedBox(width: 6),
+              PassedFilterBoxWD(),
+              SizedBox(width: 6),
+              FailedFilterBoxWD(),
+            ],
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              late final BorderRadiusGeometry borderRadius;
+              const radius = Radius.circular(6);
 
-                if (ref.watch(historyInputStateProvider).isSelectedAllResult) {
-                  borderRadius = const BorderRadius.only(topRight: radius);
-                }
-                if (ref.watch(historyInputStateProvider).isSelectedPassedResult) {
-                  borderRadius = const BorderRadius.vertical(top: radius);
-                }
-                if (ref.watch(historyInputStateProvider).isSelectedFailedResult) {
-                  borderRadius = const BorderRadius.only(topLeft: radius);
-                }
+              if (ref.watch(historyInputStateProvider).isSelectedAllResult) {
+                borderRadius = const BorderRadius.only(topRight: radius);
+              }
+              if (ref.watch(historyInputStateProvider).isSelectedPassedResult) {
+                borderRadius = const BorderRadius.vertical(top: radius);
+              }
+              if (ref.watch(historyInputStateProvider).isSelectedFailedResult) {
+                borderRadius = const BorderRadius.only(topLeft: radius);
+              }
 
-                return Flexible(
-                  child: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: borderRadius,
-                      color: DesignSystem.g1,
-                    ),
+              return Flexible(
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: borderRadius,
+                    color: DesignSystem.g1,
                   ),
-                );
-              },
-            )
-          ],
-        ),
+                ),
+              );
+            },
+          )
+        ],
       ),
     );
   }
