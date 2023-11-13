b7ec154d2ca260073666fc3eb1dec80b391e59d9
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-11-13 12:27:21 +0000

    Resolve ACDA-463 "Test/ face validation state unit test"

--- test/src/features/register/registration/logic/registration_state_notifier_test.dart
+++ test/src/features/register/registration/logic/registration_state_notifier_test.dart
@@ -22,14 +22,17 @@ class MockAPISimpleMessageModel extends Mock implements APISimpleMessageModel {}
 
 class MockRegistrationRequestModel extends Mock implements RegistrationRequestModel {}
 
+class MockAnyException extends Mock implements AnyException {}
+
 void main() {
-  late final MockRef ref;
-  late final MockRegisterUseCase registerUseCase;
-  late final MockRegisterFormInputState input;
-  late final MockICommonCategoryValueEntity faculty;
-  late final MockXFile faceImage;
-  late final MockAPISimpleMessageModel data;
-  late final Uint8List bytes;
+  late MockRef ref;
+  late MockRegisterUseCase registerUseCase;
+  late MockRegisterFormInputState input;
+  late MockICommonCategoryValueEntity faculty;
+  late MockXFile faceImage;
+  late MockAPISimpleMessageModel data;
+  late Uint8List bytes;
+  late MockAnyException anyException;
 
   setUpAll(() {
     registerFallbackValue(MockRegistrationRequestModel());
@@ -43,6 +46,7 @@ void main() {
     faceImage = MockXFile();
     data = MockAPISimpleMessageModel();
     bytes = Uint8List(10);
+    anyException = MockAnyException();
   });
 
   group('RegistrationStateNotifier', () {
@@ -79,6 +83,43 @@ void main() {
         RegistrationState.initial(),
         RegistrationState.loading(),
         RegistrationState.success(),
+      ]);
+      verify(() => registerUseCase.execute(any(that: isA<RegistrationRequestModel>()))).called(1);
+    });
+
+    test(
+        'Given unregistered user '
+        'When user register with error '
+        'Then system should throw an error', () async {
+      const email = 'test@test.com';
+      const password = 'Password123';
+      const studentIDWithU = 'u6388073';
+      const id = 1;
+      const croppedFaceImageBase64 = '<croppedFaceImageBase64>';
+
+      when(() => ref.read(registerFormInputProvider)).thenReturn(input);
+      when(() => input.faceImage).thenReturn(faceImage);
+      when(() => faceImage.readAsBytes()).thenAnswer((_) async => bytes);
+      when(() => input.croppedFaceImage).thenReturn(croppedFaceImageBase64);
+      when(() => input.email).thenReturn(email);
+      when(() => input.password).thenReturn(password);
+      when(() => input.studentIdWithU).thenReturn(studentIDWithU);
+      when(() => input.faculty).thenReturn(faculty);
+      when(() => faculty.id).thenReturn(id);
+      when(
+        () => registerUseCase.execute(any(that: isA<RegistrationRequestModel>())),
+      ).thenAnswer((_) async => Result.error(anyException));
+
+      final registrationStateNotifier = RegistrationStateNotifer(ref: ref, registerUseCase: registerUseCase);
+      final states = <RegistrationState>[];
+      registrationStateNotifier.addListener((state) => states.add(state));
+
+      await registrationStateNotifier.register();
+
+      expect(states, [
+        RegistrationState.initial(),
+        RegistrationState.loading(),
+        RegistrationState.error(),
       ]);
       verify(() => registerUseCase.execute(any(that: isA<RegistrationRequestModel>()))).called(1);
     });
