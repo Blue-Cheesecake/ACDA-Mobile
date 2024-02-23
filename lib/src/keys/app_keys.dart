dd4aaab140af3e7fdab833da8a8e85ade6ee261e
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-02-23 08:26:16 +0000

    Resolve ACDA-536 "Test/ inspect eva history"

--- lib/src/keys/app_keys.dart
+++ lib/src/keys/app_keys.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class AuthenticationKeys {
+final class AuthenticationKeys {
+  AuthenticationKeys._();
+
   static const usernameFieldWD = Key('USERNAME_FIELD_WD');
   static const passwordFieldWD = Key('PASSWORD_FIELD_WD');
   static const loginButtonWD = Key('LOGIN_BUTTON_WD');
+}
+
+final class CentralKeys {
+  CentralKeys._();
+
+  static const historyButtonNavBarWD = Key('CENTRAL_HISTORY_BOTTOM_NAV_BAR_BUTTON_WD');
+}
+
+final class HistoryPageKeys {
+  HistoryPageKeys._();
+
+  static Key getEvaluationCardWDKey(String id) => Key('HISTORY_EVA_CARD_WD_ID_$id');
+  static const backButtonFromCardPageWD = Key('HISTORY_BACK_BUTT_FOR_CARD_PAGE_WD');
 }
